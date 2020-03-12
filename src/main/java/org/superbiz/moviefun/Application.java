93e0ac91db3fb730200b901cb044a350c57ec272
Loganathan <830080@cognizant.com>
2020-03-12 11:36:36 -0400

    2 ds

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -4,8 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.superbiz.moviefun.DatabaseServiceCredentials;
+import javax.sql.DataSource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
 public class Application {
 
     public static void main(String[] args) {
@@ -16,4 +23,19 @@ public class Application {
     public ServletRegistrationBean actionServletRegistration(ActionServlet actionServlet) {
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
     }
+	
+	@Bean
+    public DatabaseServiceCredentials dbServiceCred(String vcapServices) {
+      DatabaseServiceCredentials dbServiceCred = new DatabaseServiceCredentials(vcapServices);
+    return dbServiceCred;
+   }
+	
+	@Bean
+    public DataSource albumsDataSource(DatabaseServiceCredentials serviceCredentials) {
+      MysqlDataSource dataSource = new MysqlDataSource();
+      dataSource.setURL(serviceCredentials.jdbcUrl("albums-mysql"));
+    return dataSource;
+   }
+   
+   
 }
